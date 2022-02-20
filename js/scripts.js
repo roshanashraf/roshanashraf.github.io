601a9c020269494839f970fc3c23def884d0f88c
roshanashraf <ashrafroshan245@gmail.com>
2022-02-20 16:51:03 +0530

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,39 @@
 /*!
-* Start Bootstrap - Bare v5.0.2 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Bare v5.0.2 (https://startbootstrap.com/template/bare)
+ * Copyright 2013-2021 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
 
+// var elem = document.querySelector('.grid');
+// var msnry = new Masonry( elem, {
+//   // options
+//   itemSelector: '.grid-item'
+// });
 
+// // element argument can be a selector string
+// //   for an individual element
+// var msnry = new Masonry( '.grid', {
+//   // options
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+	// do something here ...
+
+	// Images loaded is zero because we're going to process a new set of images.
+	var imagesLoaded = 0;
+	// Total images is still the total number of <img> elements on the page.
+	var totalImages = document.querySelectorAll("img").length;
+
+	
+
+	function allImagesLoaded() {
+		console.log("ALL IMAGES LOADED");
+	}
+
+
+
+	
+}, false);
